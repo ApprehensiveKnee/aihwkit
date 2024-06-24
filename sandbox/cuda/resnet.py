bd7b84e8e36b349592eb8f51ab854407996b9fa7
ApprehensiveKnee <edoardo.cabiati@mail.polimi.it>
2024-06-24 18:36:03 +0200

    reduced batch size
    reduced batch size in the test loader to cope with cuBLAS initialization problem (probably due to lack of CUDA memory -- more research probably needed)

--- sandbox/cuda/resnet.py
+++ sandbox/cuda/resnet.py
@@ -216,7 +216,7 @@ class Resnet9(torch.nn.Module):
 def resnet9s():
     return Resnet9(channels=[28, 28, 28, 56])
 
-def get_test_loader(batch_size=128):
+def get_test_loader(batch_size=32):
     transform_test = torchvision.transforms.Compose(
         [
             torchvision.transforms.ToTensor(),
