ca85238dc431975e7e7c33f9d5615b9ed213eb23
ApprehensiveKnee <edoardo.cabiati@mail.polimi.it>
2024-06-28 15:38:47 +0200

    changes to better understand accuracy trend
    Added 'plots from server' directory to store all the plots from its simulation + added some noise model and slightly changed way the testing phase is carried out in the 'resnet.py' simulation

--- sandbox/cuda/resnet.py
+++ sandbox/cuda/resnet.py
@@ -44,7 +44,6 @@ from aihwkit.simulator.parameters.enums import (
     WeightRemapType,
     WeightModifierType,
 )
-from aihwkit.nn import AnalogLinearMapped, AnalogConv2d, AnalogSequential, AnalogLinear
 # from aihwkit.utils.visualization import plot_device_compact
 # from aihwkit.utils.analog_info import analog_summary
 # from aihwkit.utils.fitting import fit_measurements
@@ -68,7 +67,7 @@ sys.path.append(t_PATH + '/sandbox/')
 import src.plotting as pl
 from src.utilities import import_mat_file
 
-from src.noise import ExperimentalNoiseModel
+from src.noise import NullNoiseModel, ExperimentalNoiseModel
 from aihwkit.inference.converter.conductance import SinglePairConductanceConverter
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -222,9 +221,9 @@ def get_test_loader(batch_size=32):
     transform_test = torchvision.transforms.Compose(
         [
             torchvision.transforms.ToTensor(),
-            torchvision.transforms.Normalize(
-                (0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)
-            ),
+            # torchvision.transforms.Normalize(
+            #     (0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)
+            # ),
         ]
     )
     testset = torchvision.datasets.CIFAR10(
@@ -278,6 +277,39 @@ class Sampler:
 # ------------------------------------------- RPU CONFIG CUSTOMIZED --------------------------------------------------
 # ********************************************************************************************************************
 
+
+@dataclass 
+class IdealPreset(InferenceRPUConfig):
+
+    mapping: MappingParameter = field(
+        default_factory=lambda: MappingParameter(
+            weight_scaling_omega=1.0,
+            weight_scaling_columnwise=False,
+            max_input_size=512,
+            max_output_size=0,
+            digital_bias=True,
+            learn_out_scaling=True,
+            out_scaling_columnwise=True,
+        )
+    )
+
+    forward: IOParameters = field(
+        default_factory= lambda:PresetIOParameters(
+            is_perfect=True,
+        )
+    )
+
+    backward: IOParameters = field(
+        default_factory= lambda:PresetIOParameters(
+            is_perfect=True,
+        )
+    )
+
+    noise_model: BaseNoiseModel = field(default_factory=NullNoiseModel)
+
+
+
+
 @dataclass
 class CustomDefinedPreset(InferenceRPUConfig):
 
@@ -313,8 +345,6 @@ class CustomDefinedPreset(InferenceRPUConfig):
     #         remapped_wmax=1.0, type=WeightRemapType.CHANNELWISE_SYMMETRIC
     #     )
     # )
-
-    #noise_model: BaseNoiseModel = field(default_factory=PCMLikeNoiseModel)
 
     # drift_compensation: Optional[BaseDriftCompensation] = field(
     #     default_factory=GlobalDriftCompensation
@@ -513,136 +543,134 @@ if __name__ == '__main__':
             )
             
 
-            
-
     accuracy_plot(model_names, inference_accuracy_values, observed_max, observed_min, n_reps ,path=p_PATH + "/resnet/plots/accuracy_resnet.png")
 
 
     # ----------------------------------Add experimental data----------------------------------
-    # print("Available experimental noises are: ", types)
-    # CHOSEN_NOISE = types[0]
-    # print(f"Chosen noise: {CHOSEN_NOISE}" )
-    # path = p_PATH+ f"/data/{MAP[SELECTED_LEVEL]}"
-    # print(f"Selected level: {SELECTED_LEVEL}")
+    print("Available experimental noises are: ", types)
+    CHOSEN_NOISE = types[0]
+    print(f"Chosen noise: {CHOSEN_NOISE}" )
+    path = p_PATH + f"/data/{MAP[SELECTED_LEVEL]}"
+    print(f"Selected level: {SELECTED_LEVEL}")
 
-    # RPU_CONFIG  = CustomDefinedPreset()
-    # RPU_CONFIG.noise_model=ExperimentalNoiseModel(file_path = path,
-    #                                             type = CHOSEN_NOISE,
-    #                                             g_converter=SinglePairConductanceConverter(g_max=40.)),
+    RPU_CONFIG  = CustomDefinedPreset()
+    RPU_CONFIG.noise_model= ExperimentalNoiseModel(file_path = path,
+                                                type = CHOSEN_NOISE,
+                                                g_converter=SinglePairConductanceConverter(g_max=40.)),
                     
 
-    # original_model = resnet9s().to(device)
-    # original_model.load_state_dict(state_dict["model_state_dict"], strict=True)
+    original_model = resnet9s().to(device)
+    original_model.load_state_dict(state_dict["model_state_dict"], strict=True)
 
-    # '''QUANTIZED 9 levels'''
-    # RPU_CONFIG.quantization = WeightQuantizerParameter(
-    #     resolution=0.2 if SELECTED_LEVEL == 9 else 0.12,
-    #     levels = SELECTED_LEVEL,
-    # )
-    # model_fitted = convert_to_analog(original_model, RPU_CONFIG)
-    # model_fitted.eval()
-    # tile_weights = next(model_fitted.analog_tiles()).get_weights()
-    # pl.plot_tensor_values(tile_weights[0], 141, (-.6,.6), f"Distribution of quantized weights - Conv1 - RESNET{SELECTED_LEVEL}", p_PATH + f"/resnet/plots/hist_resnet_QUANTIZED_{SELECTED_LEVEL}_Conv1.png")
-    # weight_max = max(abs(tile_weights[0].flatten().numpy()))
-    # model_fitted.program_analog_weights()
-
-
-    # # Plot the histogram of the weights of the last model
-    # tile_weights = next(model_fitted.analog_tiles()).get_weights()
-    # gaussain_noise = {"means": ww_mdn[CHOSEN_NOISE].values, "stds": ww_std[CHOSEN_NOISE].values, "gmax": 40.0}
-    # pl.plot_tensor_values(tile_weights[0], 141, (-.6,.6), f"Distribution of quantized weights + Fitted Noise ({CHOSEN_NOISE}) - Conv1 - RESNET{SELECTED_LEVEL}", p_PATH + f"/resnet/plots/hist_resnet_QUANTIZED_{SELECTED_LEVEL}+FITTED_Conv1.png")
-    # pl.plot_tensor_values(tile_weights[0], 141, (-.6,.6), f"Distribution of quantized weights + Fitted Noise ({CHOSEN_NOISE}) - Conv1+Gaussian \n- RESNET{SELECTED_LEVEL}", p_PATH + f"/resnet/plots/hist_resnet_QUANTIZED_{SELECTED_LEVEL}+FITTED_Conv1+Gaussian.png", gaussian=gaussain_noise, weight_max=weight_max)
-    # pl.generate_moving_hist(model_fitted,title=f"Distribution of Quantized Weight + Fitted Noise ({CHOSEN_NOISE})\n Values over the tiles - RESNET{SELECTED_LEVEL}", file_name=p_PATH + f"/resnet/plots/hist_resnet_QUANTIZED_{SELECTED_LEVEL}_FITTED.gif", range = (-.5,.5), top=None, split_by_rows=False, HIST_BINS=171)
-
-    # # Estimate the accuracy of the model with the fitted noise with respect to the other 9 levels model
-    # fitted_models_names = []
-    # fitted_models_accuracy = torch.zeros((len(t_inferences), n_reps, len(types)))
-    # fitted_observed_max = [0] * len(types)
-    # fitted_observed_min = [100] * len(types)
-    # for i in range(len(types)):
-    #     CHOSEN_NOISE = types[i]
-    #     RPU_CONFIG  = CustomDefinedPreset()
-    #     RPU_CONFIG.quantization = WeightQuantizerParameter(
-    #         resolution=0.2 if SELECTED_LEVEL == 9 else 0.12,
-    #         levels = SELECTED_LEVEL,
-    #         )
-    #     RPU_CONFIG.noise_model=ExperimentalNoiseModel(file_path = path,
-    #                                                     type = CHOSEN_NOISE,
-    #                                                     g_converter=SinglePairConductanceConverter(g_max=40.)),
-
-    #     fitted_models_names.append(f"Quantized - {SELECTED_LEVEL} levels \n+ Fitted Noise \n ({CHOSEN_NOISE})")
-    #     for t_id, t in enumerate(t_inferences):
-    #         for j in range(n_reps):
-    #             # For each repetition, get a new version of the quantized model and calibrare it
-    #             model_fitted = convert_to_analog(original_model, RPU_CONFIG)
-    #             model_fitted.eval()
-    #             model_fitted.program_analog_weights()
-
-    #             dataloader = Sampler(get_test_loader(), device)
-
-    #             calibrate_input_ranges(
-    #             model=model_fitted,
-    #             calibration_type=InputRangeCalibrationType.CACHE_QUANTILE,
-    #             dataloader=dataloader,
-    #             )
-    #             # Then evaluate the model
-    #             fitted_models_accuracy[t_id, j, i] = evaluate_model(model_fitted, get_test_loader(), device)
-    #             if fitted_observed_max[i] < fitted_models_accuracy[t_id, j, i]:
-    #                 fitted_observed_max[i] = fitted_models_accuracy[t_id, j, i]
-    #             if fitted_observed_min[i] > fitted_models_accuracy[t_id, j, i]:
-    #                 fitted_observed_min[i] = fitted_models_accuracy[t_id, j, i]
-    #             del model_fitted
-    #             del dataloader
-    #             torch.cuda.empty_cache()
-    #             gc.collect()
-    #             #torch.cuda.reset_peak_memory_stats()
-    #         print(
-    #             f"Test set accuracy (%) at t={t}s for {fitted_models_names[i]}: mean: {fitted_models_accuracy[t_id, :, i].mean()}, std: {fitted_models_accuracy[t_id, :, i].std()}"
-    #         )
-
-    # # Plot the accuracy of the models in a stem plot
-    # fig, ax = plt.subplots(figsize=(23,7))
-    # models = ["Unquantized",f"Quantized - {SELECTED_LEVEL} levels"] + fitted_models_names
-    # if SELECTED_LEVEL == 9:
-    #     accuracies = [inference_accuracy_values[t_id, :, 0].mean(),inference_accuracy_values[t_id, :, 1].mean()]
-    #     std_accuracy = [inference_accuracy_values[t_id, :, 0].std(),inference_accuracy_values[t_id, :, 1].std()]
-    #     observed_max = observed_max[:2]
-    #     observed_min = observed_min[:2]
-    # else:
-    #     accuracies = [inference_accuracy_values[t_id, :, 0].mean(),inference_accuracy_values[t_id, :, 2].mean()]
-    #     std_accuracy = [inference_accuracy_values[t_id, :, 0].std(),inference_accuracy_values[t_id, :, 2].std()]
-    #     observed_max = [observed_max[0], observed_max[2]]
-    #     observed_min = [observed_min[0], observed_min[2]]
-    # accuracies = accuracies + fitted_models_accuracy.mean(dim=1)[0].tolist()
-    # std_accuracy = std_accuracy + fitted_models_accuracy.std(dim=1)[0].tolist()
-    # observed_max = observed_max + fitted_observed_max
-    # observed_min = observed_min + fitted_observed_min
-    # ax.stem(models[:2], accuracies[:2], linefmt ='darkorange', markerfmt ='D', basefmt=' ')
-    # ax.stem(models[2:], accuracies[2:], linefmt ='darkorchid', markerfmt ='D', basefmt=' ')
-    # # Define the points for the boundary lines
-    # x = np.arange(len(models))
-    # y1 = np.array([accuracies[i] - 3*std_accuracy[i] for i in range(len(models))])
-    # y2 = np.array([accuracies[i] + 3*std_accuracy[i] for i in range(len(models))])
-    # max = np.array(observed_max)
-    # min = np.array(observed_min)
-    # # Interpolating or directly using the points to fill the region
-    # ax.fill_between(x, y1, y2, where=(y2 > y1), color='bisque', alpha=0.5, label='Confidence Interval')
-    # ax.plot(x, y1, '--', color='firebrick')
-    # ax.plot(x, y2, '--', color = 'firebrick')
-    # ax.fill_between(x, min, max, where=(max > min), color='lightsalmon', alpha=0.5, label='Observed Accuracy Interval')
-    # ax.plot(x, max, ls='dashdot', color = 'olivedrab', label = 'Max observed accuracy', marker = '1', markersize=10)
-    # ax.plot(x, min, ls= 'dashdot', color = 'olivedrab', label = 'Min observed accuracy', marker = '2', markersize=10)
+    '''QUANTIZED 9 levels'''
+    RPU_CONFIG.quantization = WeightQuantizerParameter(
+        resolution=0.2 if SELECTED_LEVEL == 9 else 0.12,
+        levels = SELECTED_LEVEL,
+    )
+    model_fitted = convert_to_analog(original_model, RPU_CONFIG)
+    model_fitted.eval()
+    tile_weights = next(model_fitted.analog_tiles()).get_weights()
+    pl.plot_tensor_values(tile_weights[0], 141, (-.6,.6), f"Distribution of quantized weights - Conv1 - RESNET{SELECTED_LEVEL}", p_PATH + f"/resnet/plots/hist_resnet_QUANTIZED_{SELECTED_LEVEL}_Conv1.png")
+    weight_max = max(abs(tile_weights[0].flatten().numpy()))
+    model_fitted.program_analog_weights()
 
 
-    # ax.set_title(f"Accuracy of the models over {n_reps} repetitions")
-    # ax.set_ylabel("Accuracy (%)")
-    # ax.set_xlim([-0.5, len(models)- 0.5])
-    # ax.minorticks_on()
-    # ax.yaxis.grid(True)
-    # ax.yaxis.grid(which='minor', linestyle=':', linewidth='0.5', color='gray')
-    # ax.set_ylim([50, 90])
-    # ax.legend()
-    # # Save the plot to file
-    # plt.savefig(p_PATH+f"/resnet/plots/accuracy_resnet_FittedNoise_{SELECTED_LEVEL}.png")
+    # Plot the histogram of the weights of the last model
+    tile_weights = next(model_fitted.analog_tiles()).get_weights()
+    gaussain_noise = {"means": ww_mdn[CHOSEN_NOISE].values, "stds": ww_std[CHOSEN_NOISE].values, "gmax": 40.0}
+    pl.plot_tensor_values(tile_weights[0], 141, (-.6,.6), f"Distribution of quantized weights + Fitted Noise ({CHOSEN_NOISE}) - Conv1 - RESNET{SELECTED_LEVEL}", p_PATH + f"/resnet/plots/hist_resnet_QUANTIZED_{SELECTED_LEVEL}+FITTED_Conv1.png")
+    pl.plot_tensor_values(tile_weights[0], 141, (-.6,.6), f"Distribution of quantized weights + Fitted Noise ({CHOSEN_NOISE}) - Conv1+Gaussian \n- RESNET{SELECTED_LEVEL}", p_PATH + f"/resnet/plots/hist_resnet_QUANTIZED_{SELECTED_LEVEL}+FITTED_Conv1+Gaussian.png", gaussian=gaussain_noise, weight_max=weight_max)
+    pl.generate_moving_hist(model_fitted,title=f"Distribution of Quantized Weight + Fitted Noise ({CHOSEN_NOISE})\n Values over the tiles - RESNET{SELECTED_LEVEL}", file_name=p_PATH + f"/resnet/plots/hist_resnet_QUANTIZED_{SELECTED_LEVEL}_FITTED.gif", range = (-.5,.5), top=None, split_by_rows=False, HIST_BINS=171)
+
+    # Estimate the accuracy of the model with the fitted noise with respect to the other 9 levels model
+    fitted_models_names = []
+    fitted_models_accuracy = torch.zeros((len(t_inferences), n_reps, len(types)))
+    fitted_observed_max = [0] * len(types)
+    fitted_observed_min = [100] * len(types)
+    for i in range(len(types)):
+        CHOSEN_NOISE = types[i]
+        RPU_CONFIG  = CustomDefinedPreset()
+        RPU_CONFIG.quantization = WeightQuantizerParameter(
+            resolution=0.2 if SELECTED_LEVEL == 9 else 0.12,
+            levels = SELECTED_LEVEL,
+            )
+        RPU_CONFIG.noise_model=ExperimentalNoiseModel(file_path = path,
+                                                        type = CHOSEN_NOISE,
+                                                        g_converter=SinglePairConductanceConverter(g_max=40.)),
+
+        fitted_models_names.append(f"Quantized - {SELECTED_LEVEL} levels \n+ Fitted Noise \n ({CHOSEN_NOISE})")
+        for t_id, t in enumerate(t_inferences):
+            for j in range(n_reps):
+                # For each repetition, get a new version of the quantized model and calibrare it
+                model_fitted = convert_to_analog(original_model, RPU_CONFIG)
+                model_fitted.eval()
+                model_fitted.program_analog_weights()
+
+                dataloader = Sampler(get_test_loader(), device)
+
+                calibrate_input_ranges(
+                model=model_fitted,
+                calibration_type=InputRangeCalibrationType.CACHE_QUANTILE,
+                dataloader=dataloader,
+                )
+                # Then evaluate the model
+                fitted_models_accuracy[t_id, j, i] = evaluate_model(model_fitted, get_test_loader(), device)
+                if fitted_observed_max[i] < fitted_models_accuracy[t_id, j, i]:
+                    fitted_observed_max[i] = fitted_models_accuracy[t_id, j, i]
+                if fitted_observed_min[i] > fitted_models_accuracy[t_id, j, i]:
+                    fitted_observed_min[i] = fitted_models_accuracy[t_id, j, i]
+                del model_fitted
+                del dataloader
+                torch.cuda.empty_cache()
+                gc.collect()
+                #torch.cuda.reset_peak_memory_stats()
+            print(
+                f"Test set accuracy (%) at t={t}s for {fitted_models_names[i]}: mean: {fitted_models_accuracy[t_id, :, i].mean()}, std: {fitted_models_accuracy[t_id, :, i].std()}"
+            )
+
+    # Plot the accuracy of the models in a stem plot
+    fig, ax = plt.subplots(figsize=(23,7))
+    models = ["Unquantized",f"Quantized - {SELECTED_LEVEL} levels"] + fitted_models_names
+    if SELECTED_LEVEL == 9:
+        accuracies = [inference_accuracy_values[t_id, :, 0].mean(),inference_accuracy_values[t_id, :, 1].mean()]
+        std_accuracy = [inference_accuracy_values[t_id, :, 0].std(),inference_accuracy_values[t_id, :, 1].std()]
+        observed_max = observed_max[:2]
+        observed_min = observed_min[:2]
+    else:
+        accuracies = [inference_accuracy_values[t_id, :, 0].mean(),inference_accuracy_values[t_id, :, 2].mean()]
+        std_accuracy = [inference_accuracy_values[t_id, :, 0].std(),inference_accuracy_values[t_id, :, 2].std()]
+        observed_max = [observed_max[0], observed_max[2]]
+        observed_min = [observed_min[0], observed_min[2]]
+    accuracies = accuracies + fitted_models_accuracy.mean(dim=1)[0].tolist()
+    std_accuracy = std_accuracy + fitted_models_accuracy.std(dim=1)[0].tolist()
+    observed_max = observed_max + fitted_observed_max
+    observed_min = observed_min + fitted_observed_min
+    ax.stem(models[:2], accuracies[:2], linefmt ='darkorange', markerfmt ='D', basefmt=' ')
+    ax.stem(models[2:], accuracies[2:], linefmt ='darkorchid', markerfmt ='D', basefmt=' ')
+    # Define the points for the boundary lines
+    x = np.arange(len(models))
+    y1 = np.array([accuracies[i] - 3*std_accuracy[i] for i in range(len(models))])
+    y2 = np.array([accuracies[i] + 3*std_accuracy[i] for i in range(len(models))])
+    max = np.array(observed_max)
+    min = np.array(observed_min)
+    # Interpolating or directly using the points to fill the region
+    ax.fill_between(x, y1, y2, where=(y2 > y1), color='bisque', alpha=0.5, label='Confidence Interval')
+    ax.plot(x, y1, '--', color='firebrick')
+    ax.plot(x, y2, '--', color = 'firebrick')
+    ax.fill_between(x, min, max, where=(max > min), color='lightsalmon', alpha=0.5, label='Observed Accuracy Interval')
+    ax.plot(x, max, ls='dashdot', color = 'olivedrab', label = 'Max observed accuracy', marker = '1', markersize=10)
+    ax.plot(x, min, ls= 'dashdot', color = 'olivedrab', label = 'Min observed accuracy', marker = '2', markersize=10)
+
+
+    ax.set_title(f"Accuracy of the models over {n_reps} repetitions")
+    ax.set_ylabel("Accuracy (%)")
+    ax.set_xlim([-0.5, len(models)- 0.5])
+    ax.minorticks_on()
+    ax.yaxis.grid(True)
+    ax.yaxis.grid(which='minor', linestyle=':', linewidth='0.5', color='gray')
+    ax.set_ylim([50, 90])
+    ax.legend()
+    # Save the plot to file
+    plt.savefig(p_PATH+f"/resnet/plots/accuracy_resnet_FittedNoise_{SELECTED_LEVEL}.png")
 
 
--- sandbox/src/noise.py
+++ sandbox/src/noise.py
@@ -19,6 +19,14 @@ class NullNoiseModel(BaseNoiseModel):
 
     def apply_programming_noise_to_conductance(self, g_target: torch.Tensor) -> torch.Tensor:
         return g_target
+    
+    def generate_drift_coefficients(self, g_target: torch.Tensor) ->torch.Tensor:
+        """Not used"""
+        return torch.tensor(0.)
+     
+    def apply_drift_noise_to_conductance(self, g_prog, t_inference) -> torch.Tensor:
+        """Apply drift up to the assumed inference time"""
+        return g_prog 
 
     def __str__(self) -> str:
         return self.__class__.__name__
@@ -83,7 +91,7 @@ class ExperimentalNoiseModel(BaseNoiseModel):
         return torch.tensor(0.)
      
     def apply_drift_noise_to_conductance(self, g_prog, t_inference) -> torch.Tensor:
-        """Apply drift up to the assumed inference time"""
+        """No drift noise is applied in this model"""
         return g_prog 
     
     @no_grad()
@@ -145,9 +153,12 @@ class ExperimentalNoiseModel(BaseNoiseModel):
         # First, determine the quantization level each conductance belongs to
         g_max = g_target.max() if g_target.max() > -g_target.min() else -g_target.min()
         if ww_mdn.shape == ww_std.shape: # Check on identical shapes
+            # gg_values will represent the possible conductance values
             #gg_values = torch.unique(gg_mdn)
             gg_values = [-g_max + i * 2 * g_max / (ww_mdn.shape[0]-1)  for i in range(ww_mdn.shape[0])]
             gg_values = torch.tensor(gg_values)
+        else:
+            raise ValueError("The median and standard deviation tensors must have the same shape")
         # Determine the quantization level each conductance belongs to
         g_real = torch.zeros_like(g_target)
 
@@ -166,3 +177,81 @@ class ExperimentalNoiseModel(BaseNoiseModel):
         return g_real
 
     
+
+class JustMedianNoiseModel(ExperimentalNoiseModel):
+    """ 
+    This new noise model just considers the median values of the experimental data:
+    the conductances are shifted from their original quantized values to the corresponding median values
+    """
+    def __init__(self, file_path: str, type: str, **kwargs):
+        super().__init__(file_path, type, **kwargs)
+
+    @staticmethod
+    def fit_data(g_target, ww_mdn, ww_std):
+        """ Differently from the handle of the parent class, this function
+        just considers the median values of the experimental data.
+
+        Args:
+            g_target: the target conductances tensor
+            gg_mdn: the tensor median of the conductances
+            gg_std: the tensor standard deviation of the conductances
+        
+        Returns:
+            g_real: the fittted conductances tensor
+        """
+
+        g_max = g_target.max() if g_target.max() > -g_target.min() else -g_target.min()
+        if ww_mdn.shape == ww_std.shape: # Check on identical shapes
+            gg_values = [-g_max + i * 2 * g_max / (ww_mdn.shape[0]-1)  for i in range(ww_mdn.shape[0])]
+            gg_values = torch.tensor(gg_values)
+        else:
+            raise ValueError("The median and standard deviation tensors must have the same shape")
+        g_real = torch.zeros_like(g_target)
+
+        
+        diffs = torch.abs(gg_values.unsqueeze(-1) - g_target.reshape(-1))
+        min_indices = torch.argmin(diffs, dim=0)
+        g_real = ww_mdn[min_indices]
+        g_real = g_real.reshape(g_target.shape)
+        return g_real
+
+
+class JustStdNoiseModel(ExperimentalNoiseModel):  
+    """ 
+    This new noise model just considers the standard deviation values of the experimental data:
+    the conductances are shifted from their original quantized values to the corresponding standard deviation values
+    """
+
+    def __init__(self, file_path: str, type: str, **kwargs):
+        super().__init__(file_path, type, **kwargs)
+
+    @staticmethod
+    def fit_data(g_target, ww_mdn, ww_std):
+        """ Differently from the handle of the parent class, this function
+        just considers the standard deviation values of the experimental data.
+
+        Args:
+            g_target: the target conductances tensor
+            gg_mdn: the tensor median of the conductances
+            gg_std: the tensor standard deviation of the conductances
+        
+        Returns:
+            g_real: the fittted conductances tensor
+        """
+
+        g_max = g_target.max() if g_target.max() > -g_target.min() else -g_target.min()
+        if ww_mdn.shape == ww_std.shape: # Check on identical shapes
+            gg_values = [-g_max + i * 2 * g_max / (ww_mdn.shape[0]-1)  for i in range(ww_mdn.shape[0])]
+            gg_values = torch.tensor(gg_values)
+        else:
+            raise ValueError("The median and standard deviation tensors must have the same shape")
+        g_real = torch.zeros_like(g_target)
+
+        
+        diffs = torch.abs(gg_values.unsqueeze(-1) - g_target.reshape(-1))
+        min_indices = torch.argmin(diffs, dim=0)
+        g_real = ww_std[min_indices] * randn_like(g_target.reshape(-1))
+        g_real = g_real.reshape(g_target.shape)
+        return g_real
+
+
