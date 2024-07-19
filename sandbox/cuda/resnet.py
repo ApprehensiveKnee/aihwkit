1f01cdb3342b2726ebeed5c958e8d54dfa04243b
ApprehensiveKnee <edoardo.cabiati@mail.polimi.it>
2024-07-19 13:55:14 +0200

    Update resnet.py

--- sandbox/cuda/resnet.py
+++ sandbox/cuda/resnet.py
@@ -255,8 +255,8 @@ if __name__ == '__main__':
     # The state dict of the model with hardware-aware trained weights is stored in the
     # model_state_dict key of the external checkpoint.
     model.load_state_dict(state_dict["model_state_dict"], strict=True)
-    rpu_config = IdealPreset()
-    model = convert_to_analog(model, IdealPreset())
+    rpu_config = CustomDefinedPreset()
+    model = convert_to_analog(model, rpu_config)
     model.eval()
     pl.generate_moving_hist(model,title="Distribution of Weight Values over the tiles - RESNET", file_name=p_PATH+"/resnet/plots/hist_resnet_UNQUATIZED.gif", range = (-.5,.5), top=None, split_by_rows=False, HIST_BINS = 171)
 
@@ -330,7 +330,7 @@ if __name__ == '__main__':
     path = p_PATH + f"/data/{MAP_LEVEL_FILE[SELECTED_LEVEL]}"
     print(f"Selected level: {SELECTED_LEVEL}")
 
-    RPU_CONFIG  = IdealPreset()
+    RPU_CONFIG  = CustomDefinedPreset()
     RPU_CONFIG.noise_model= MAP_NOISE_TYPE[SELECTED_NOISE](file_path = path,
                                                             type = CHOSEN_NOISE,
                                                             levels = SELECTED_LEVEL,
@@ -383,7 +383,7 @@ if __name__ == '__main__':
 
     for i in range(len(types)):
         CHOSEN_NOISE = types[i]
-        RPU_CONFIG  = IdealPreset()
+        RPU_CONFIG  = CustomDefinedPreset()
         RPU_CONFIG.quantization = WeightQuantizerParameter(
             resolution=0.2 if SELECTED_LEVEL == 9 else 0.12,
             levels = SELECTED_LEVEL,
