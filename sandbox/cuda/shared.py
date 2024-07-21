c1f86aa9b0630554c2b95893d8294d6b40e274e9
ApprehensiveKnee <edoardo.cabiati@mail.polimi.it>
2024-07-21 22:16:00 +0200

    Update shared.py

--- sandbox/cuda/shared.py
+++ sandbox/cuda/shared.py
@@ -334,8 +334,8 @@ class IdealPreset(InferenceRPUConfig):
             # w_noise=0.0175,
             w_noise_type=WeightNoiseType.NONE,
             ir_drop=1.0,
-            #out_noise=0.04,
-            #out_bound=10.0,
+            out_noise=0.04,
+            out_bound=10.0,
         )
     )
 
