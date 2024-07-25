3b8511f3fabe2d197137d518cb4c4a8ea6bc4076
ApprehensiveKnee <edoardo.cabiati@mail.polimi.it>
2024-07-25 20:24:45 +0200

    Update levels_comparison.py

--- sandbox/cuda/levels_comparison.py
+++ sandbox/cuda/levels_comparison.py
@@ -211,7 +211,7 @@ if __name__ == '__main__':
 
     
     # Prepare first the ideal models
-    model_accuracy = torch.zeros((len(LEVELS)+1,len(types)+1,N_REPS))
+    model_accuracy = torch.zeros((2 if COMPENSATION else 1,len(LEVELS)+1,len(types)+1, N_REPS))
     models_ideal = ["Unquantized","Quantized - 3 levels", "Quantized - 5 levels", "Quantized - 9 levels", "Quantized - 17 levels", "Quantized - 33 levels",]
     unquantized_model = sel_model_init(SELECTED_MODEL, RPU_CONFIG_BASE, state_dict)
 
@@ -243,8 +243,8 @@ if __name__ == '__main__':
                 pl.generate_moving_hist(model_i, title=f"{model_name} - {SELECTED_NOISE}", file_name=f"{p_PATH}/{SELECTED_MODEL}/plots/Weight_Distribution_comparison_plots/{model_name}_{SELECTED_NOISE}.gif",  range = (-.7,.7), top=None, split_by_rows=False)
 
 
-            model_accuracy[i,0,j] = evaluate_model(model_i, get_test_loader(), device)
-            print(f"Model: {model_name} - Repetition: {j} - Accuracy: {model_accuracy[i,0,j]}")
+            model_accuracy[0,i,0,j] = evaluate_model(model_i, get_test_loader(), device)
+            print(f"Model: {model_name} - Repetition: {j} - Accuracy: {model_accuracy[0,i,0,j]}")
 
             del model_i
             if SELECTED_MODEL == "resnet":
@@ -252,7 +252,7 @@ if __name__ == '__main__':
             torch.cuda.empty_cache()
             gc.collect()
 
-        print(f"Model: {model_name} - Average accuracy: {model_accuracy[i,0,:].mean()}, std: {model_accuracy[i,0,:].std() if N_REPS > 1 else 0}")
+        print(f"Model: {model_name} - Average accuracy: {model_accuracy[0,i,0,:].mean()}, std: {model_accuracy[0,i,0,:].std() if N_REPS > 1 else 0}")
     
     # Now execute the rest of the evaluations for the different types of noise
     if not os.path.exists(f"{p_PATH}/{SELECTED_MODEL}/plots/Conv1_comparison_plots"):
@@ -260,63 +260,74 @@ if __name__ == '__main__':
     else:
         os.system(f"rm -r {p_PATH}/{SELECTED_MODEL}/plots/Conv1_comparison_plots")
         os.mkdir(f"{p_PATH}/{SELECTED_MODEL}/plots/Conv1_comparison_plots")
-                    
-    for i, levels in enumerate(LEVELS):
-        for j, noise_type in enumerate(types):
-            RPU_CONFIG = deepcopy(RPU_CONFIG_BASE)
-            RPU_CONFIG.noise_model=MAP_NOISE_TYPE[SELECTED_NOISE](file_path = p_PATH + f"/data/{MAP_LEVEL_FILE[levels]}",
-                                                        type = types[j],
-                                                        levels = levels,
-                                                        force_interpolation = True,
-                                                        g_converter=SinglePairConductanceConverter(g_max=40.))
-            
-            for k in range(N_REPS):
-                model = sel_model_init(SELECTED_MODEL, RPU_CONFIG, state_dict)
-                model = get_quantized_model(model, levels, RPU_CONFIG, eps=EPS)
-                model.eval()
-                model.program_analog_weights()
 
-                if k == 0:
-                    tile_weights = next(model.analog_tiles()).get_weights()
-                    pl.plot_tensor_values(tile_weights[0], 141, (-.6,.6), f"Conv1 {SELECTED_MODEL} - levels={levels} - noise_type={noise_type}", p_PATH + f"/{SELECTED_MODEL}/plots/Conv1_comparison_plots/Conv1-levels={levels}-type={noise_type}.png")
+    for h in range(2 if COMPENSATION else 1):             
+        for i, levels in enumerate(LEVELS):
+            for j, noise_type in enumerate(types):
+                RPU_CONFIG = deepcopy(RPU_CONFIG_BASE)
+                RPU_CONFIG.noise_model=MAP_NOISE_TYPE[SELECTED_NOISE](file_path = p_PATH + f"/data/{MAP_LEVEL_FILE[levels]}",
+                                                            type = types[j],
+                                                            levels = levels,
+                                                            force_interpolation = True,
+                                                            compensation = False if h == 0 else True,
+                                                            g_converter=SinglePairConductanceConverter(g_max=40.))
+                
+                for k in range(N_REPS):
+                    model = sel_model_init(SELECTED_MODEL, RPU_CONFIG, state_dict)
+                    model = get_quantized_model(model, levels, RPU_CONFIG, eps=EPS)
+                    model.eval()
+                    model.program_analog_weights()
 
-                if SELECTED_MODEL == "resnet":
-                    # Calibrate input ranges
-                    dataloader = Sampler(get_test_loader(), device)
-                    calibrate_input_ranges(
-                    model=model,
-                    calibration_type=InputRangeCalibrationType.CACHE_QUANTILE,
-                    dataloader=dataloader,
-                    )
+                    if k == 0:
+                        tile_weights = next(model.analog_tiles()).get_weights()
+                        pl.plot_tensor_values(tile_weights[0], 141, (-.6,.6), f"Conv1 {SELECTED_MODEL} - levels={levels} - noise_type={noise_type}", p_PATH + f"/{SELECTED_MODEL}/plots/Conv1_comparison_plots/Conv1-levels={levels}-type={noise_type}.png")
 
-                model_accuracy[i+1,j+1,k] = evaluate_model(model, get_test_loader(), device)
-                #print(f"Model: {levels} levels - Noise: {noise_type} - Repetition: {k} - Accuracy: {model_accuracy[i+1,j+1,k]}")
+                    if SELECTED_MODEL == "resnet":
+                        # Calibrate input ranges
+                        dataloader = Sampler(get_test_loader(), device)
+                        calibrate_input_ranges(
+                        model=model,
+                        calibration_type=InputRangeCalibrationType.CACHE_QUANTILE,
+                        dataloader=dataloader,
+                        )
 
-                del model
-                if SELECTED_MODEL == "resnet":
-                    del dataloader
-                torch.cuda.empty_cache()
-                gc.collect()
+                    model_accuracy[h,i+1,j+1,k] = evaluate_model(model, get_test_loader(), device)
+                    #print(f"Model: {levels} levels - Noise: {noise_type} - Repetition: {k} - Accuracy: {model_accuracy[h, i+1,j+1,k]}")
 
-            print(
-                f"Model: {levels} levels - Noise: {noise_type} - Average accuracy: {model_accuracy[i+1,j+1,:].mean()}, std: {model_accuracy[i+1,j+1,:].std() if N_REPS > 1 else 0}"
-            )
+                    del model
+                    if SELECTED_MODEL == "resnet":
+                        del dataloader
+                    torch.cuda.empty_cache()
+                    gc.collect()
+
+                print(
+                    f"Model: {levels} levels - Noise: {noise_type} - Average accuracy: {model_accuracy[h,i+1,j+1,:].mean()}, std: {model_accuracy[h,i+1,j+1,:].std() if N_REPS > 1 else 0}"
+                )
 
     # Plot and save the results
 
     fig, ax = plt.subplots(1,1, figsize=(23,7))
     accuracies = np.zeros((len(LEVELS),len(types)+1))
-    accuracy_unquantized = model_accuracy[0,0,:].mean()
+    accuracy_unquantized = model_accuracy[0,0,0,:].mean()
     for i in range(1,len(LEVELS)+1):
         for j in range(len(types)+1):
-            accuracies[i-1,j] = model_accuracy[i,j,:].mean()
+            accuracies[i-1,j] = model_accuracy[0,i,j,:].mean()
+    if COMPENSATION:
+        accuracies_comp = np.zeros((len(LEVELS),len(types)+1))
+        for i in range(1,len(LEVELS)+1):
+            for j in range(len(types)+1):
+                if j == 0:
+                    accuracies_comp[i-1,j] = model_accuracy[0,i,j,:].mean()
+                else:
+                    accuracies_comp[i-1,j] = model_accuracy[1,i,j+1,:].mean()
+
     
-    colors = plt.get_cmap('inferno')
-    norm = Normalize(0, len(LEVELS))
+    colors = ["coral", "plum"]
     markers = ['o','s','^','v','D']
     x = np.arange(len(types)+1)
-    for i in range(1,len(LEVELS)+1):
-        ax.plot(x, accuracies[i-1,:], label=f"{LEVELS[i-1]} levels", color=colors(norm(i-1)), marker=markers[i-1])
+    for h in range(2 if COMPENSATION else 1):
+        for i in range(1,len(LEVELS)+1):
+            ax.plot(x, accuracies[i-1,:] if h == 0 else accuracies_comp[i-1,:], label=f"{LEVELS[i-1]} levels", color=colors[h], marker=markers[i-1], markerfacecolor='black', markeredgecolor='black')
     ax.plot(x, [accuracy_unquantized for _ in range(len(types)+1)], label="Unquantized", color="black", linestyle="--")
     # Save the plot
     ax.set_xticks(x, labels=['No Noise']+types)
@@ -324,28 +335,33 @@ if __name__ == '__main__':
     ax.set_ylabel("Accuracy")
     ax.set_title("Accuracy comparison between q.levels at different noise types")
     ax.legend()
-    plt.savefig(f"{p_PATH}/{SELECTED_MODEL}/plots/accuracy_level_comparison_{SELECTED_NOISE}.png")
+    plt.savefig(f"{p_PATH}/{SELECTED_MODEL}/plots/level_comparison_{SELECTED_MODEL}_{SELECTED_NOISE}.png")
 
     # Also plot a heatmap
-    fig, ax = plt.subplots(1,1, figsize=(23,23))
-    plt.rcParams.update({'font.size': 22})
-    im = ax.matshow(accuracies, cmap='viridis',origin='lower' )
-    for (i,j), z in np.ndenumerate(accuracies):
-        ax.text(j, i, '{:0.1f}'.format(z), ha='center', va='center', color='white',
-            bbox=dict(boxstyle='round', facecolor='black', edgecolor='black'))
-    im_ratio = accuracies.shape[0]/accuracies.shape[1]
-    cax = fig.add_axes([ax.get_position().x1+0.01,ax.get_position().y0,0.02,ax.get_position().height])
-    cbar = plt.colorbar(im, cax=cax)
-    cbar.ax.set_ylabel("reached accuracy", rotation=-90, va="bottom")
-    ax.spines[:].set_visible(False)
-    ax.set_xticks(np.arange(len(types)+1), labels=['No Noise']+types)
-    ax.set_yticks(np.arange(len(LEVELS)), labels=LEVELS)
-    plt.setp(ax.get_xticklabels(), rotation=40, ha="left", va="bottom",  fontsize = 18, rotation_mode="anchor")
-    plt.setp(ax.get_yticklabels(), fontsize = 18)
-    ax.set_xlabel("Noise type", fontsize=22)
-    ax.set_ylabel("Levels", fontsize=22)
-    ax.set_title("Accuracy comparison between q.levels at different noise types", pad=45)
-    plt.savefig(f"{p_PATH}/{SELECTED_MODEL}/plots/heatmap_accuracy_level_comparison_{SELECTED_NOISE}.png")
+    for h in range(2 if COMPENSATION else 1):
+        fig, ax = plt.subplots(1,1, figsize=(23,23))
+        text = ""
+        if h != 0:
+            accuracies = accuracies_comp 
+            text = "w/comp"
+        plt.rcParams.update({'font.size': 22})
+        im = ax.matshow(accuracies, cmap='viridis',origin='lower' )
+        for (i,j), z in np.ndenumerate(accuracies):
+            ax.text(j, i, '{:0.1f}'.format(z), ha='center', va='center', color='white',
+                bbox=dict(boxstyle='round', facecolor='black', edgecolor='black'))
+        im_ratio = accuracies.shape[0]/accuracies.shape[1]
+        cax = fig.add_axes([ax.get_position().x1+0.01,ax.get_position().y0,0.02,ax.get_position().height])
+        cbar = plt.colorbar(im, cax=cax)
+        cbar.ax.set_ylabel("accuracy range", rotation=-90, va="bottom")
+        ax.spines[:].set_visible(False)
+        ax.set_xticks(np.arange(len(types)+1), labels=['No Noise']+types)
+        ax.set_yticks(np.arange(len(LEVELS)), labels=LEVELS)
+        plt.setp(ax.get_xticklabels(), rotation=40, ha="left", va="bottom",  fontsize = 18, rotation_mode="anchor")
+        plt.setp(ax.get_yticklabels(), fontsize = 18)
+        ax.set_xlabel("Noise type", fontsize=22)
+        ax.set_ylabel("Levels", fontsize=22)
+        ax.set_title(f"Accuracy comparison between q.levels at different noise types({text})", pad=45)
+        plt.savefig(f"{p_PATH}/{SELECTED_MODEL}/plots/heatmap_accuracy_level_{SELECTED_MODEL}_{SELECTED_NOISE}_{text}.png")
 
 
     
