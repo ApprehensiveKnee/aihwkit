7348812f5df241fe34e16d6c593f54feacd76508
ApprehensiveKnee <edoardo.cabiati@mail.polimi.it>
2024-11-04 20:10:58 +0100

    updates

--- src/aihwkit/simulator/tiles/periphery.py
+++ src/aihwkit/simulator/tiles/periphery.py
@@ -202,7 +202,7 @@ class TileWithPeriphery(BaseTile, SimulatorTileWrapper):
                         alert = "===============================================================\n"
                         alert += f"WARNING: in {self.tile.__class__.__name__} tile bias:\n"
                         alert += f"Weight quantizer produced {np.unique(quant_bias).size} levels, but {new_wqpar.levels} were requested.\n"
-                        alert += f"The unique levels are: {np.unique(quant_weights)}\n"
+                        alert += f"The unique levels are: {np.unique(quant_bias)}\n"
                         alert += "===============================================================\n"
                         print(alert)
 
--- src/rpucuda/cuda/weight_quantizer_cuda.cu
+++ src/rpucuda/cuda/weight_quantizer_cuda.cu
@@ -145,8 +145,7 @@ void WeightQuantizerCuda<T>::apply(T *weights, const WeightQuantizerParameter<T>
     switch (wqpar.quantizer_type) {
         case WeightQuantizerType::UniformSymmetric: {
             if (wqpar.resolution >0){
-              T z = (T).0;
-              std::cout << "The levels are: " << wqpar.levels << std::endl; 
+              T z = (T).0; 
                 
               // call the kernel
               kernelQuantize<T><<<nblocks, nthreads, 0, s>>>(
