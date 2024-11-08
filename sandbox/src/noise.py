f9ddc5919c0febd26c722bdf141619be8e4e3c6a
ApprehensiveKnee <edoardo.cabiati@mail.polimi.it>
2024-11-08 12:41:19 +0100

    Update noise.py

--- sandbox/src/noise.py
+++ sandbox/src/noise.py
@@ -84,9 +84,11 @@ class InterpolatedNoiseModel(BaseNoiseModel):
     def apply_programming_noise_to_conductance(self, g_target: torch.Tensor) -> torch.Tensor:
         """Apply programming noise using the polynomial fitting coefficients"""
 
+        print(g_target[0,:10])
         g_prog = torch.zeros_like(g_target)
         p_mdn , p_std = self.mdn_p[0], self.std_p[0]
         g_prog = torch.tensor(p_mdn(g_target)) + torch.tensor(p_std(g_target)) * randn_like(g_target)
+        print(g_prog[0,:10])
         return g_prog
     
     def generate_drift_coefficients(self, g_target: torch.Tensor) ->torch.Tensor:
